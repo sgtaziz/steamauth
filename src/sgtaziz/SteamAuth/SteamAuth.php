127585a724b3d9cc46b03b1d92530b328228ff00
Aziz Hasanain <sgtaziz013@gmail.com>
2015-03-13 16:45:35 -0400

    started from the bottom

--- src/sgtaziz/SteamAuth/SteamAuth.php
+++ src/sgtaziz/SteamAuth/SteamAuth.php
@@ -1,7 +1,24 @@
 <?php
 namespace sgtaziz\SteamAuth;
 
+require_once "Libraries/OpenID.php";
+use LightOpenID;
+
 class SteamAuth
 {
-	
+	private $OpenIDURL = 'https://steamcommunity.com/openid';
+	private $OpenID = null;
+
+	public function __construct()
+	{
+		$this->OpenID = new LightOpenID($this->OpenIDURL);
+	}
+
+	public function Auth($returnUrl)
+	{
+		$this->OpenID->identity = $this->OpenIDURL;
+		$this->OpenID->returnUrl = $returnUrl;
+
+		/*location shit here*/
+	}
 }
--- tests/test.php
+++ tests/test.php
@@ -2,3 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use sgtaziz\SteamAuth\SteamAuth;
+
+$test = new SteamAuth;
+$test->Auth('server2.horizonservers.net');
