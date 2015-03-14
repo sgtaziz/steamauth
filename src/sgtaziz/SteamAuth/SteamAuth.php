f034782369227fe8f967a4685a31cb2cbe33573e
Aziz Hasanain <sgtaziz013@gmail.com>
2015-03-14 01:32:32 -0400

    miles is a loser :/

--- src/sgtaziz/SteamAuth/SteamAuth.php
+++ src/sgtaziz/SteamAuth/SteamAuth.php
@@ -8,8 +8,8 @@ use SimpleXMLElement;
 
 class SteamAuth
 {
-	private $OpenIDURL = 'https://steamcommunity.com/openid';
-	private $OpenID = null;
+	private $OpenIDURL		= 'https://steamcommunity.com/openid';
+	private $OpenID			= null;
 
 	public function __construct()
 	{
@@ -35,44 +35,13 @@ class SteamAuth
 
 			if ($steamid64)
 			{
-				$xmlraw = file_get_contents( "http://steamcommunity.com/profiles/".$steamid64."/?xml=1" ); 
-				$xml = new SimpleXMLElement($xmlraw);
+				$json = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . config('steamauth.SteamAPIKey') . '&steamids=' . $steamid64); 
+				$json = json_decode($json, true);
 
-				if (is_object($xml))
+				$user = $json["response"]["players"][0];
+
+				if ($user)
 				{
-					$httpsURL = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/';
-					preg_match('/^(.*)?\/avatars\/(.*)$/i', (string)$xml->{'avatarMedium'}, $newAvatarLink);
-					preg_match('/^(.*)?\/avatars\/(.*)$/i', (string)$xml->{'avatarFull'}, $newAvatarFullLink);
-					preg_match('/^(.*)?\/avatars\/(.*)$/i', (string)$xml->{'avatarIcon'}, $newAvatarSmallLink);
-					$avatar = $httpsURL.$newAvatarLink[2];
-					$avatarFull = $httpsURL.$newAvatarFullLink[2];
-					$avatarSmall = $httpsURL.$newAvatarSmallLink[2];
-
-					$user = (object)array();
-					$user->steamid64 = (string)$xml->{'steamID64'};
-					$user->name = (string)$xml->{'steamID'};
-					$user->state = (string)$xml->{'onlineState'};
-					$user->avatar = $avatar;
-					$user->avatarFull = $avatarFull;
-					$user->avatarSmall = $avatarSmall;
-					$user->private = (int)$xml->{'visibilityState'} != 3;
-					$user->vacbans = (string)$xml->{'vacBanned'};
-					$user->tradebans = (string)$xml->{'tradeBanState'};
-					$user->limited = (int)$xml->{'isLimitedAccount'} == 1;
-					$user->customURL = (string)$xml->{'customURL'};
-					$user->ingame = isset($xml->{'ingameInfo'});
-
-					if ($user->ingame)
-					{
-						$user->ingamename = (string)$xml->{'gameName'};
-						$user->ingamelink = (string)$xml->{'gameLink'};
-						$user->ingameicon = (string)$xml->{'gameIcon'};
-						$user->ingamelogo = (string)$xml->{'gameLogo'};
-						$user->ingamelogosmall = (string)$xml->{'gameLogoSmall'};
-					}
-
-					$user->created = (string)$xml->{'memberSince'};
-
 					return $user;
 				}
 			}
