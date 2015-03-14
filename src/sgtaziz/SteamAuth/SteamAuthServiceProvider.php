e35c50663a53302fbdcba98a3ae890967b11bc2f
Aziz Hasanain <sgtaziz013@gmail.com>
2015-03-13 22:13:58 -0400

    now we here

--- src/sgtaziz/SteamAuth/SteamAuthServiceProvider.php
+++ src/sgtaziz/SteamAuth/SteamAuthServiceProvider.php
@@ -1,7 +1,31 @@
 <?php
 namespace sgtaziz\SteamAuth;
 
-class SteamAuthServiceProvider
+use Illuminate\Support\ServiceProvider;
+
+class SteamAuthServiceProvider extends ServiceProvider
 {
-	
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['steamauth'] = $this->app->share(
+			function ($app) {
+				return new SteamAuth;
+			}
+		);
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('steamauth');
+	}
 }
